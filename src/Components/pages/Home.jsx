b1fd9eb056a72699142149d86949f449694968f8
3steban99 <barrionuevo99@outlook.com>
2024-04-11 21:05:10 -0300

    estructura basica del home

---
+++ src/Components/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { Container } from "react-bootstrap";
+import Carousels from "../sections/Carousel";
+import Categories from "../sections/Categories";
+import News from "../sections/News";
+
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <Carousels />
+            <Container>
+                <News />
+                <div className="d-flex justify-content-center mb-5">
+                    <img src="https://www.venex.com.ar/fil/banners/armadorpclargo.jpg" alt="banner" />
+                </div>
+                <Categories />
+            </Container>
+        </div>
+    )
+}
+
+
+export default Home;
