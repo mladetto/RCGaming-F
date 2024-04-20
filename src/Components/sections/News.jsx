e059fb5fb9e3f0fb4f597896f876b8cee17cf1f3
3steban99 <barrionuevo99@outlook.com>
2024-04-19 21:26:00 -0300

    mostrar productos destacados

--- src/Components/sections/News.jsx
+++ src/Components/sections/News.jsx
@@ -3,11 +3,31 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 
 
 const News = () => {
+    const [news, setNews] = useState([])
+
+    const API = import.meta.env.VITE_API;
+
+    useEffect(() => {
+        getProductsNews();
+    }, []);
+
+    async function getProductsNews() {
+        try {
+            const resp = await axios.get(`${API}/products/news`);
+            setNews(resp.data);
+        } catch (error) {
+            throw new Error("Error al obtener los productos destacados" + error.message);
+        }
+    }
+    console.log("productos->",news)
+
     return (
         <Container>
             <h2>Destacados</h2>
@@ -33,16 +53,12 @@ const News = () => {
                 }}
                 className='swiper-news'
             >
-                <SwiperSlide className='swiper-card'>Slide 1</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 2</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-                <SwiperSlide className='swiper-card'>Slide 3</SwiperSlide>
-
+                {news.map((product, index) => (
+                    <SwiperSlide key={index} className='swiper-card shadow'>
+                        <img src={product.imageUrl} alt={product.name} />
+                        <h5>{product.name}</h5>
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </Container>
     )
