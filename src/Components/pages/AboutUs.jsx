8178665151da85e06f002f8a91d52860fcda5ff3
jimena <herrerajimena01@gmail.com>
2024-04-28 21:50:10 -0300

    se agregaron imagenes y descripciones de los integrantes

--- src/Components/pages/AboutUs.jsx
+++ src/Components/pages/AboutUs.jsx
@@ -1,8 +1,11 @@
-import imagen from "../../img/fotor_1713971778404.jpg"
+import felix from "../../img/felix.jpeg"
+import martin from"../../img/e30111ec-b5c1-439a-8cfe-3ef3adfe036d.jpeg"
+import esteban from "../../img/_1e3ba369-0c65-4f60-9ee4-677660d118ab.jpg"
+import jimena from "../../img/OIG3.jpeg"
 import  "../pages/AboutUs.css"
 import {Col} from "react-bootstrap"
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Row from 'react-bootstrap/Row'
 
 
 const AboutUs = () => {
@@ -14,41 +17,41 @@ const AboutUs = () => {
             <p className="text-center p-2 mx-3 fs-5 text-dark">Somos estudiantes de la Comisión 80i y miembros del Grupo 4.Estamos presentando nuestro proyecto final para terminar con el curso de full stcak. A continuación, les presentamos una breve descripción de cada uno:</p>
         </div>
         </Container>
-        <Container className="overflow-x-hidden">
+        <Container fluid className="overflow-x-hidden">
         <Row >
         <Col xs={12} md={6} lg={4}>
-        <div className="text-center  my-3  container px-3">
-          <img src={imagen} alt="Avatar Jimena" className="img " />
-          <h5 className="my-2 fw-bold">Jimena Herrera</h5>
-          <p className="text-center ">Tengo 23 años..</p>
+        <div className="text-center  my-3 mx-3  container p-3">
+          <img src={martin} alt="Avatar Martin Ladetto " className="img  rounded-circle " />
+          <h5 className="my-2 fw-bold">Martin Ladetto</h5>
+          <p className="text-center ">Soy Martin Ladetto, tengo 34 años, me gusta el ciclismo y los deportes al aire libre. Actualmente trabajo en una empresa en el area de IT y soy estudiante de RollingCode.</p>
         </div>
         </Col>
         <Col xs={12} md={6} lg={4}>
-        <div className="text-center  my-3  container px-3 ">
-          <img src={imagen} alt="Avatar Jimena" className="img " />
-          <h5 className="my-2 fw-bold">Jimena Herrera</h5>
-          <p className="text-center ">Tengo 23 años..</p>
+        <div className="text-center  my-3 mx-3 container p-3 ">
+          <img src={felix} alt="Avatar Felix Lamas" className="img rounded-circle " />
+          <h5 className="my-2 fw-bold">Felix Lamas</h5>
+          <p className="text-center ">Soy Felix ELoy Lamas, tengo 35 años, soy padre de dos hermosas niñas, me gusta el futbol y el ciclismo, actualmente estudiante en RollingCode y en la UNT.</p>
         </div>
         </Col>
        <Col>
-        <div className="text-center  my-3  container px-3">
-          <img src={imagen} alt="Avatar Jimena" className="img " />
-          <h5 className="my-2 fw-bold">Jimena Herrera</h5>
-          <p className="text-center ">Tengo 23 años..</p>
+        <div className="text-center  my-3 mx-3  container p-3">
+          <img src={esteban} alt="Avatar Esteban Barrionuevo" className=" img rounded-circle " />
+          <h5 className="my-2 fw-bold">Esteban Barrionuevo</h5>
+          <p className="text-center ">Soy Esteban Barrionuevo, tengo 25 años, soy graduado en la tecnicatura en programación UTN y actualmente trabajo en Telematica como soporte IT. Mis hobbies son entrenar y jugar.</p>
         </div>
         </Col>
         <Col>
-        <div className="text-center  my-3  container px-3">
-          <img src={imagen} alt="Avatar Jimena" className="img " />
+        <div className="text-center  my-3 mx-3 container p-3">
+          <img src={jimena} alt="Avatar Jimena Herrera" className=" img rounded-circle" />
           <h5 className="my-2 fw-bold">Jimena Herrera</h5>
-          <p className="text-center ">Tengo 23 años..</p>
+          <p className="text-center ">Soy Jimena Herrea tengo 23 años, actualmente soy estudiante de Rolling Code, mis hobbies son leer, ver series y jugar con mi perra. </p>
         </div>
         </Col>
         <Col>
-        <div className="text-center  my-3  container px-3">
-          <img src={imagen} alt="Avatar Jimena" className="img " />
-          <h5 className="my-2 fw-bold">Jimena Herrera</h5>
-          <p className="text-center ">Tengo 23 años..</p>
+        <div className="text-center  my-3 mx-3  container p-3">
+          <img src={jimena} alt="Avatar Rodrigo Cardoso" className=" img rounded-circle" />
+          <h5 className="my-2 fw-bold">Rodrigo Cardoso</h5>
+          <p className="text-center ">Soy Rodrigo Cardoso </p>
         </div>
         </Col>
         </Row>
